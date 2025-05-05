fac934eba44e2400166d7054719fd5de868f2c39
Aletmf <trause.alessandro@gmail.com>
2025-05-05 06:39:54 -0300

    Adição Curso II

---
+++ Curso II/Arrays/array-de-arrays.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias]
+
+console.log(lista);
+
+console.log(lista[0][1], lista[1][1]);
+
+const teste = alunos.concat('5');
+console.log(alunos);
---
+++ Curso II/Arrays/atualizando-listas.js
@@ -0,0 +1,7 @@
+const estudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+//Precisamos retirar e adicionar itens nesse array. Utilizaremos o método splice
+
+estudantes.splice(1, 2, 'Rodrigo');
+               //Onde começará o delete, quantos elementos a partir desse serão deletados, elemento a ser "pusahado"
+console.log(estudantes);
+
